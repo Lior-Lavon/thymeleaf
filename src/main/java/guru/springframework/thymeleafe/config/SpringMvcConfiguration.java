d6fce5d245a274091573d229e33f93fe9dfbac3d
Lior Lavon <>
2020-06-02 23:32:21 +0200

    adding i18n support based on ?lang=en

---
+++ src/main/java/guru/springframework/thymeleafe/config/SpringMvcConfiguration.java
@@ -0,0 +1,53 @@
+package guru.springframework.thymeleafe.config;
+
+import org.apache.tomcat.util.descriptor.LocalResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class SpringMvcConfiguration extends WebMvcConfigurerAdapter {
+
+    // Run as: ...url?lang=en
+
+
+    // set default local to US
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
+
+    // set a language throw the 'lang' local variable
+    // if not available/set then use the Local.US default
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry){
+        interceptorRegistry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:locale/messages");
+        messageSource.setCacheSeconds(3600); // refresh cache once per hour
+        return messageSource;
+    }
+
+}
